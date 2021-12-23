d632904b17c2962815b38289c138bb92b0fcdcd9
Bandi Revanth <dhanush24082014@gmail.com>
2021-12-23 13:04:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("Welcome to Tic Tac Toe")
+console.log("Welcome to Tic Tac Toe!")
 let music = new Audio("music.mp3")
 let audioTurn = new Audio("ting.mp3")
 let gameover = new Audio("gameover.mp3")
